61a5d25c843d98bd520b57f66ef375e584118d3d
shaymaa khalaf <shaymaa.khalaf91@gmail.com>
2025-08-20 20:23:46 +0300

    Statistical Analysis R-Shiny App

---
+++ R Shiny - STAT503/R Shiny - STAT503/STAT503/Link_code.R
@@ -0,0 +1,4 @@
+install.packages('rsconnect')
+rsconnect::setAccountInfo(name='fwks', token='E834C19517F7408FE8AA567429AB4FDD', secret='I1InW+dDjozAPAdcXSuERHphok30D+BPbV7naqXp')
+library(rsconnect)
+rsconnect::deployApp("C:/Users/FAB/iCloudDrive/KFUPM/2/STAT503/R shiny/R Shiny - STAT503_final/R Shiny - STAT503/STAT503")
